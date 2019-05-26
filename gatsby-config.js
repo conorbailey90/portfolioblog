15aeeb6f623d6069b79f925e8df95c5517cb8886
Conor Bailey <conorbailey@conors-mbp.home>
2019-05-26 10:58:58 +0100

    Second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
     author: `Conor Bailey`,
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
